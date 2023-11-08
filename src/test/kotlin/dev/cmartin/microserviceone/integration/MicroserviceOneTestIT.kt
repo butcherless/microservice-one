082abf919af5f0129dea2b47e6db3f0fc35cc64b
Carlos Martin <cmarting@gmail.com>
2023-11-08 10:33:45 +0100

    upgrade plugins

--- src/test/kotlin/dev/cmartin/microserviceone/integration/MicroserviceOneTestIT.kt
+++ src/test/kotlin/dev/cmartin/microserviceone/integration/MicroserviceOneTestIT.kt
@@ -1,5 +1,6 @@
 package dev.cmartin.microserviceone.integration
 
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,7 +9,7 @@ class MicroserviceOneTestIT {
 
     @Test
     fun testFailsafePlugin() {
-        assert(true)
+        assertTrue(true)
     }
 
 }
