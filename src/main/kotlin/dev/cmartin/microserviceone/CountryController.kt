a3abb0b6999877b9d4459b1761444b84860ee925
Carlos Martin <cmarting@gmail.com>
2023-12-03 12:58:08 +0100

    add find by id endppoint

--- src/main/kotlin/dev/cmartin/microserviceone/CountryController.kt
+++ src/main/kotlin/dev/cmartin/microserviceone/CountryController.kt
@@ -4,7 +4,9 @@ import dev.cmartin.microserviceone.Model.Code
 import dev.cmartin.microserviceone.Model.Country
 import dev.cmartin.microserviceone.Model.Name
 import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
@@ -22,6 +24,19 @@ class CountryController {
             .toFlux()
     }
 
+    @GetMapping("$countries/{code}")
+    fun getCountryByCode(@PathVariable code: String): ResponseEntity<Country> {
+        logger.debug("retrieving country by code: $code")
+
+        val list = getCountries()
+            .filter { it.code.value == code }
+
+        return when {
+            list.isNotEmpty() -> ResponseEntity.ok(list.first())
+            else -> ResponseEntity.notFound().build()
+        }
+    }
+
     companion object {
         val logger = LoggerFactory.getLogger(Companion::class.java.name)
         const val countries = "countries"
@@ -35,12 +50,15 @@ class CountryController {
         val frName = Name("France")
         val deCode = Code("de")
         val deName = Name("Germany")
+        val ukCode = Code("uk")
+        val ukName = Name("United Kingdom")
 
         val esCountry = Country(esCode, esName)
         val ptCountry = Country(ptCode, ptName)
         val itCountry = Country(itCode, itName)
         val frCountry = Country(frCode, frName)
         val deCountry = Country(deCode, deName)
+        val ukCountry = Country(ukCode, ukName)
 
         fun getCountries(): List<Country> =
             listOf(
@@ -48,7 +66,8 @@ class CountryController {
                 ptCountry,
                 itCountry,
                 frCountry,
-                deCountry
+                deCountry,
+                ukCountry
             )
     }
 }
