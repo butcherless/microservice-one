92c429c84b790955510800a45fca1547d6c15302
Carlos Martin <cmarting@gmail.com>
2023-12-08 13:23:29 +0100

    refactor code

--- src/main/kotlin/dev/cmartin/microserviceone/CountryController.kt
+++ src/main/kotlin/dev/cmartin/microserviceone/CountryController.kt
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
+
 @RestController
 @RequestMapping("ms-one/countries")
 class CountryController(private val countryService: CountryService) {
@@ -27,13 +28,13 @@ class CountryController(private val countryService: CountryService) {
         Flux.from(
             this.countryService
                 .findByName(name)
-                .switchIfEmpty(Mono.error(CountryNotFoundException("$name not found")))
-                .also { logger.debug("get country by name: $name") }
+                .handleMissingCountryError(name)
+                .also { logger.debug("$GET_BY_NAME: $name") }
         )
 
     private fun getAllSorted(orderBy: String): Flux<Country> =
         this.countryService.findAll(resolveSortableProperty(orderBy))
-            .also { logger.debug("get countries order by: $orderBy") }
+            .also { logger.debug("$GET_ORDER_BY: $orderBy") }
 
 
     @GetMapping("/{code}")
@@ -41,13 +42,20 @@ class CountryController(private val countryService: CountryService) {
         return Flux.from(
             this.countryService
                 .findByCode(code)
-                .switchIfEmpty(Mono.error(CountryNotFoundException("$code not found")))
-                .also { logger.debug("get country by code: $code") }
+                .handleMissingCountryError(code)
+                .also { logger.debug("$GET_BY_CODE: $code") }
         )
     }
 
     companion object {
         val logger: Logger = LoggerFactory.getLogger(CountryController::class.java)
+
+        private const val GET_BY_CODE = "get country by code"
+        private const val GET_BY_NAME = "get country by name"
+        private const val GET_ORDER_BY = "get countries order by"
+
+        private fun Mono<Country>.handleMissingCountryError(identifier: String) =
+            this.switchIfEmpty(Mono.error(CountryNotFoundException("$identifier not found")))
 
         private fun resolveSortableProperty(orderBy: String): SortableProperties =
             when (orderBy.uppercase()) {
