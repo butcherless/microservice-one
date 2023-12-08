0bbb176b888591182d52c741d12f2e77b685c1f7
Carlos Martin <cmarting@gmail.com>
2023-12-08 17:40:19 +0100

    add javadoc

--- src/main/kotlin/dev/cmartin/microserviceone/ApplicationUtils.kt
+++ src/main/kotlin/dev/cmartin/microserviceone/ApplicationUtils.kt
@@ -5,6 +5,12 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.core.io.ClassPathResource
 
 object ApplicationUtils {
+    /**
+     * Reads a JSON file and converts it into a list of [Model.Country] objects.
+     *
+     * @param path The path to the JSON file.
+     * @return A list of [Model.Country] objects.
+     */
     fun readJsonFile(path: String): List<Model.Country> =
         jacksonObjectMapper()
             .readValue<List<Model.Country>>(ClassPathResource(path).inputStream)
--- src/main/kotlin/dev/cmartin/microserviceone/Model.kt
+++ src/main/kotlin/dev/cmartin/microserviceone/Model.kt
@@ -9,9 +9,26 @@ object Model {
     @JvmInline
     value class Name(val value: String)
 
+    /**
+     * Represents a country with its code and name.
+     *
+     * @property code The code of the country.
+     * @property name The name of the country.
+     */
     data class Country(val code: String, val name: String)
 
+    /**
+     * Exception thrown when a country is not found.
+     *
+     * @property message The error message.
+     */
     data class CountryNotFoundException(override val message: String) : RuntimeException(message)
 
+    /**
+     * Represents an error response containing a message and the instant it occurred.
+     *
+     * @property message The error message.
+     * @property instant The instant the error occurred.
+     */
     data class ErrorResponse(val message: String, val instant: Instant)
 }
