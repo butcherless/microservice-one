1b68f736d4ca8bd71c3e5f78898fee9f91e2ef23
Carlos Martin <cmarting@gmail.com>
2023-12-10 13:21:56 +0100

    refactor code

--- src/test/kotlin/dev/cmartin/microserviceone/TestData.kt
+++ src/test/kotlin/dev/cmartin/microserviceone/TestData.kt
@@ -1,11 +1,12 @@
 package dev.cmartin.microserviceone
 
+import dev.cmartin.microserviceone.CountryService.Companion.SortableProperties
 import dev.cmartin.microserviceone.Model.Country
 
 object TestData {
     val spain = Country("es", "Spain")
     val france = Country("fr", "France")
     val countries: List<Country> = listOf(france, spain)
-    val sortByName = CountryService.Companion.SortableProperties.NAME
-    val sortByCode = CountryService.Companion.SortableProperties.CODE
+    val sortByName = SortableProperties.NAME
+    val sortByCode = SortableProperties.CODE
 }
